8abce0de99f3575abef665fabea8afb09c7a5bf0
IronManMark20 <mr.smittye@gmail.com>
2015-04-20 14:43:30 -0700

    functional v0.0.5

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import ctypes
 from ctypes import wintypes
 from collections import namedtuple
 import thread
-import time
+__version__="0.0.5"
 KeyEvents=namedtuple("KeyEvents",(['event_type', 'key_code',
 											 'scan_code', 'alt_pressed',
 											 'time']))
@@ -19,9 +19,8 @@ class hook:
 		self.current_keys=[]
 		#Scancodes and a few key codes
 		self.keylist=["Null","Esc","1","2","3","4","5","6","7","8","9","0","-","=","Backspace","Tab","Q","W","E","R","T","Y","U","I","O","P","[","]","Return","LCtrl","A","S","D","F","G","H","J","K","L",";","'","`","LShift","\\","Z","X","C","V","B","N","M",",",".","/","RShift","Key*","LAlt","Space","Capslock","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","Numlock","ScrollLock","KeyHome","Up","KeyPgUp","Key-","Left","Key5","Right","Key+","End","Down","KeyPgDn","KeyIns","KeyDel","SYSRQ","","","F11","F12","","","LWin","RWin","MenuKey","RAlt","RCtrl"]
-		#For a convention, I will take care of key codes only when needed, as I already started with scan codes (shift taken care of)
-		#(NOTE: In Decimal!)Key=scan_code,key_code; LAlt=56,164; RAlt=56,165; SYSRQ=55,44;Key*=55,106; Numlock=Unk,255; LCtrl=29,162;RCtrl=29,163
 	def print_event(self,e):
+		"""This parses through the keyboard events. You shouldn't ever need this."""
 		func_called=False
 		self.estr=str(e)
 		start=self.estr.index("scan_code=")
@@ -53,10 +52,9 @@ class hook:
 		if str(e.event_type)=="key down" and not (key in self.current_keys):
 			self.current_keys.append(key)
 			for id in self.IDs:
-				if self.current_keys==id[1] or self.current_keys in id[1]:
-					ind=id[1].index(self.current_keys)
+				if self.current_keys==id[1]:
 					if not func_called:
-						id[2][ind]()
+						id[2]()
 					func_called=True
 		#remove when released
 		elif str(e.event_type)=="key up":
@@ -68,9 +66,9 @@ class hook:
 	def Hotkey(self,list=[],fhot=None):
 		"""Adds a new hotkey. Definition: Hotkey(list=[],fhot=None) where list is the list of
 		keys and fhot is the callback function"""
-		self.list+=[list]
-		self.fhot+=[fhot]
-		self.IDs.append([self.oldID,self.list,self.fhot])
+		#self.list=[list]
+		#self.fhot=[fhot]
+		self.IDs.append([self.oldID,list,fhot])
 		self.oldID+=1
 		if self.list is [] or self.fhot is None:
 			raise Exception("Error: Empty key list or no callback function")
@@ -79,25 +77,18 @@ class hook:
 			return (self.oldID-1)
 	def RemHotKey(self,hkey):
 		"""Remove a hotkey. Specify the id, the key list, or the function to remove the hotkey."""
-		print hkey, self.IDs
-		'''if str(type(hkey))=="<type 'int'>":
-			print "__int__"
-			for hk in self.IDs:
-				for id in hk[0]:
-					if hk==hkey:
-						self.IDs.remove(hk)
+		if str(type(hkey))=="<type 'int'>":
+			for hotk in self.IDs:
+					if hotk[0]==hkey:
+						self.IDs.remove(hotk)
 		elif str(type(hkey))=="<type 'list'>":
-			print "__list__"
-			for hk in self.IDs:
-				if hk[1]==hkey:
-					self.IDs.remove(hk)
+			for hotk in self.IDs:
+				if hotk[1]==hkey:
+					self.IDs.remove(hotk)
 		elif str(type(hkey))=="<type 'function'>":
-			print "__func__"
-			for hk in self.IDs:
-				print hk[2]
-				if hk[2]==hkey:
-					print "found"
-					self.IDs.remove(hk)'''
+			for hotk in self.IDs:
+				if hotk[2]==hkey:
+					self.IDs.remove(hotk)
 	def listener(self):
 		"""The listener listens to events and adds them to handlers"""
 		from ctypes import windll, CFUNCTYPE, POINTER, c_int, c_void_p, byref
@@ -136,12 +127,14 @@ class hook:
 		"""Start listening for hooks"""
 		self.listener()
 def foo():
+	"""For the example, it prints 'foo'."""
 	print "foo"
 def foobar():
+	"""For the example, it prints 'foobar'."""
 	print "foobar"
 if __name__ == '__main__':
 	hk=hook()
-	hk.Hotkey(["LCtrl","A"],foo)
-	hk.Hotkey(["LCtrl","B"],foobar)
-	hk.RemHotKey(foobar)
+	hk.Hotkey(["LCtrl","A"],foo) # hotkey 0
+	hk.Hotkey(["LCtrl","B"],foobar) #hotkey 1
+	hk.RemHotKey(1) # or you could use hk.RemHotKey(["LCtrl","B"]) or hk.RemHotKey(foobar)
 	hk.listen()
