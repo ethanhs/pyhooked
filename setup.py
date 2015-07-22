e6f7f83948426b9c9a304f442142c6ce60232d5b
John Smith <rgbbyte@gmail.com>
2015-07-21 14:05:13 -1000

    release to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+name='pyhooked'
+packages=['pyhooked']
+version='0.7'
+description='Pure Python hotkey hook, with thanks to pyHook and pyhk'
+author='Ethan Smith'
+author_email='mr.smittye@gmail.com'
+url='https://github.com/IronManMark20/hooked'
+download_url=""
+keywords = ['hotkey','shortcut','windows','keyboard','hooks']
+classifiers = [],
+)
