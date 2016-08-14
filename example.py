0496fd5169e5c719201bd9c9981a83b37a43549f
Ethan <mr.smittye@gmail.com>
2016-08-14 09:26:47 -0700

    add mouse example

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from pyhooked import Hook, KeyboardEvent
+from pyhooked import Hook, KeyboardEvent, MouseEvent
 
 
 def handle_events(args):
@@ -7,7 +7,10 @@ def handle_events(args):
         if args.current_key == 'A' and args.event_type == 'key down' and 'Lcontrol' in args.pressed_key:
             print("Ctrl + A was pressed")
 
+    if isinstance(args, MouseEvent):
+        if args.mouse_x == 300 and args.mouse_y == 400:
+            print("Mouse is at (300,400") 
 
 hk = Hook()  # make a new instance of PyHooked
-hk.handler = handle_events  # add a new shortcut ctrl+a, that calls foo() when pressed
+hk.handler = handle_events  # add a new shortcut ctrl+a, or triggered on mouseover of (300,400)
 hk.hook()  # hook into the events, and listen to the presses
