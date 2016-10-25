1b6ca8180aec3c7df9c6b846fbe5771458e2272a
Ethan <ethan@ethanhs.me>
2016-10-24 17:15:08 -0700

    fix url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Pure Python hotkey hook, with thanks to pyHook, pywinauto, and pyhk',
     author='Ethan Smith',
     author_email='ethan@ethanhs.me',
-    url='https://github.com/ethanhs/hooked',
+    url='https://github.com/ethanhs/pyhooked',
     download_url="https://github.com/ethanhs/pyhooked/tarball/0.8.1",
     keywords=['hotkey', 'shortcut', 'windows', 'keyboard', 'hooks', 'hook'],
     classifiers=[],
